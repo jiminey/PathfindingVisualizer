d88a578806be63e21d466e3f9aad2377556d694d
jiminey <jtwnguyen@ucdavis.edu>
2019-11-11 21:26:56 -0800

    Create nodes and start point/endpoint

--- src/Node/Node.jsx
+++ src/Node/Node.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Node.css";
 
 export default class Node extends Component {
-    constructor(props){
-        super(props)
-        this.state = {}
-    }
-    render() {
-        return (
-            <div className="node">
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { isStart, isFinish } = this.props;
+    //triple ternary
+    const endPointNode = isFinish ? "node-finish" : isStart ? "node-start" : "";
+
+    return <div className={`node ${endPointNode}`}></div>;
+  }
 }
+
+export const DEFAULT_NODE = {
+  row: 0,
+  col: 0
+};
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,40 +1,56 @@
-import React, { Component } from 'react'
-import Node from '../Node/Node'
+import React, { Component } from "react";
+import Node from "../Node/Node";
+
+import "./PathfindingVisualizer.css";
 
 export default class PathfindingVisualizer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            grid: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: []
+    };
+  }
 
-    componentDidMount() {
-        //initialize grid
-        const grid = []
-        for (let row = 0; row < 20; row++) {
-            const currentRow = []
-            for (let col = 0; col < 50; col++) {
-                currentRow.push([])
-            }
-            grid.push(currentRow)
-        }
-        this.setState({grid})
+  componentDidMount() {
+    //initialize grid
+    const grid = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5, //isStart return boolean if row=10 col=5
+          isFinish: row === 15 && col === 45
+        };
+        currentRow.push(currentNode);
+      }
+      grid.push(currentRow);
     }
-    
+    this.setState({ grid });
+  }
 
-    render() {
-        const {grid} = this.state
-        return (
-            <div className='grid'>
-                {grid.map((row, rowIdx) => {
-                    return (
-                        <div>
-                            {row.map((node, nodeIdx) => <Node></Node> )}
-                        </div>
-                    )
-                })}
+  render() {
+    const { grid } = this.state;
+    return (
+      <div className="grid">
+        {grid.map((row, rowIdx) => {
+          return (
+            <div>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  ></Node>
+                );
+              })}
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
