5890a43dfaaa056d71fff06741daee5530b66b5b
jiminey <jtwnguyen@ucdavis.edu>
2019-11-13 10:18:47 -0800

    Add logic for neighboring nodes

--- src/Algorithms/dijkstras.js
+++ src/Algorithms/dijkstras.js
@@ -18,6 +18,16 @@ function dijkstra(grid, startNode, endNode) {
         const closestNode = sortedUnvisitedNodes.shift();
 
         //if encounter a wall we skip it aka move around
+        //maybe add a isWall
+
+        //if surronding nodes is infinity then just return visitedNodes
+        if (closestNode.distance === Infinity) return visitedNodes
+
+        closestNode.isVisited = true;
+        visitedNodes.push(closestNode)
+
+        if (closestNode === finishNode) return visitedNodes;
+        updateUnvisitedNeighbors(closestNode, grid);
     }
 
     //set distance to every node to infinity -- too far away
@@ -27,6 +37,19 @@ function dijkstra(grid, startNode, endNode) {
     //grab closest node of all remaing node, pick one of the 4 and keep doing this process 
 
 
+}
+
+function updateUnvisitedNeighbors(node, grid) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid)
+}
+
+function getUnvisitedNeighbors(node, grid) {
+    const neighbors = []
+    const {row, col} = node
+
+    if (row > 0) neighbors.push(grid[row - 1][col]) //top
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]) // bottom
+    if (col > 0) neighbors.push(grid[row][col - 1])
 }
 
 function sortNodesByDistance(unvistedNodes) {
