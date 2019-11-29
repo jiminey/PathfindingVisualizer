1116b580a1659896db57d9de34e4bbdfb47c03f8
jiminey <jtwnguyen@ucdavis.edu>
2019-11-28 23:37:20 -0800

    Complete astar fucntion

--- src/Algorithms/astar.js
+++ src/Algorithms/astar.js
@@ -33,9 +33,20 @@ export function astar(grid, startNode, endNode) {
             if (!openList.includes(neighbor)) {
                 //save g, h, f then save the currentParent
                 isGCostBest = true;
-                neighbor.h = heuristic(startNode.x, startNode.y, endNode.x, endNode.y)
+                neighbor.hCost = manhattanHeuristic(startNode.x, startNode.y, endNode.x, endNode.y)
                 openList.push(neighbor)
+            } else if (gCost < neighbor.gCost) {
+                // We have already seen the node, but last time it had a worse g (distance from start)
+                isGCostBest = true; 
             }
+
+            if (isGCostBest) {
+                neighbor.gCost = gCost
+                neighbor.fCost = neighbor.gCost + neighbor.hCost
+                neighbor.previousNode = closestNode;
+            }
+
+
             if (openList.includes(neighbor) 
             // && current g is better than previous g
             ) {
@@ -57,7 +68,7 @@ export function astar(grid, startNode, endNode) {
     }
 }
 
-function heuristic(startX, startY, endX, endY) {
+function manhattanHeuristic(startX, startY, endX, endY) {
     // Utilize the Manhattan Distance -- allows for 4 directional movement (up, down, left, right)
     return Math.abs(startX - endX) + Math.abs(startY - endY)
 }
