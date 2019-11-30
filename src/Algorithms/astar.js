da7b869067608e812861857241400bdee239581e
jiminey <jtwnguyen@ucdavis.edu>
2019-11-29 22:33:30 -0800

    Attempt astar refractor

--- src/Algorithms/astar.js
+++ src/Algorithms/astar.js
@@ -1,49 +1,56 @@
-import { getUnvisitedNeighbors, getAllNodes } from './dijkstras' 
+import { getUnvisitedNeighbors } from './dijkstras' 
 
 export function astar(grid, startNode, endNode) {
 
     // Initialize
     const closedList = [];
-    startNode.distance = 0;
     startNode.gCost = 0;
+    startNode.fCost = 0;
+    startNode.hCost = 0;
     endNode.hCost = 0
     const openList = [startNode];
 
     while(!!openList.length) {
-        sortByFCost(openList)
+        sortNodesByFCost(openList)
         const closestNode = openList.shift();
 
-        closestNode.isVisited = true;
         closedList.push(closestNode)
 
-        if (closestNode.isWall) continue;
+        // if (closestNode.fCost = Infinity) return closedList; 
 
-        // check for trap?
-        // if (closestNode.distance = Infinity) return closedList; 
+        if (closestNode === endNode) return closedList; 
 
-        if (currentNode === endNode) return closedList; 
-
-        updateUnvisitedNeighbors(closestNode, grid)
+        updateUnvisitedNeighbors(closestNode, grid, startNode, endNode, closedList, openList)
     }
 }
 
-function updateUnvisitedNeighbors(node, grid, startNode, endNode) {
+function updateUnvisitedNeighbors(node, grid, startNode, endNode, closedList, openList) {
     const unvisitedNeighbors = getUnvisitedNeighbors(node, grid)
     for (const neighbor of unvisitedNeighbors) {
         // neighbor is in closedList
-        if (closedList.includes(neighbor)) continue; 
+        if (closedList.includes(neighbor) || neighbor.isWall) continue; 
         
-        // set g, h , and f 
-        neighbor.gCost = node.gCost + 1
-        neighbor.hCost = manhattanHeuristic(startNode.x, startNode.y, endNode.x, endNode.y)
-        neighbor.fCost = neighbor.gCost + neighbor.hCost
-        neighbor.previousNode = node
-
         // if neighbor is already in openList
-        if (openList.includes(neighbor)) continue; 
         
+        let isBestPath = false;
+        let tempF = node.gCost + 1
+        
+        if (openList.includes(neighbor)) {
+            if (tempF <= neighbor.fCost) {
+                neighbor.fCost = tempF
+                isBestPath = true
+            }
+        }  else {
+            neighbor.gCost = node.gCost + 1
+            neighbor.hCost = manhattanHeuristic(startNode.x, startNode.y, endNode.x, endNode.y)
+            neighbor.fCost = neighbor.gCost + neighbor.hCost
+            isBestPath = true
+            openList.push(neighbor);
+        }
+        // set g, h , and f 
+
         // append neighbor to open list
-        openList.push(neighbor);
+        if (isBestPath) neighbor.previousNode = node
     }
 }
 
@@ -52,6 +59,6 @@ function manhattanHeuristic(startX, startY, endX, endY) {
     return Math.abs(startX - endX) + Math.abs(startY - endY)
 }
 
-function sortByFCost(unvisitedNodes) {
+function sortNodesByFCost(unvisitedNodes) {
     unvisitedNodes.sort((nodeA,nodeB) => nodeA.fCost - nodeB.fCost)
 }
