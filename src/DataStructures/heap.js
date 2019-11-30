d2e6353787f91014b71fbba9ba4cee0eaae6d5b8
jiminey <jtwnguyen@ucdavis.edu>
2019-11-30 04:50:49 -0800

    Create all functions for heap

--- src/DataStructures/heap.js
+++ src/DataStructures/heap.js
@@ -2,5 +2,28 @@ export default class MinHeap {
     constructor(array){
         this.heap = this.buildHeap(array);
     }
-    
+    buildHeap(array){
+
+    }
+    siftDown(currentIdx, endIdx, heap){
+
+    }
+
+    siftUp(currentIdx, heap){
+
+    }
+    peek() {
+        return this.heap[0]
+    }
+
+    remove() {
+
+    }
+    insert() { 
+
+    }
+
+    swap(i ,j, heap) {
+        [heap[i], heap[j]] = [heap[j], heap[i]]
+    }
 }
