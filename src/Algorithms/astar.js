881e4d24b3cb4ee16ebd343738fbc2db230b0529
jiminey <jtwnguyen@ucdavis.edu>
2019-11-29 20:57:12 -0800

    Reset astar fuction refractor

--- src/Algorithms/astar.js
+++ src/Algorithms/astar.js
@@ -1,48 +1,32 @@
 import { getUnvisitedNeighbors, getAllNodes } from './dijkstras' 
 
-
 export function astar(grid, startNode, endNode) {
-    const closedList = [];
-    startNode.fCost = 0
-    const openList = [startNode];
+   const visitedNodes = []
+   startNode.distance = 0;
+   const unvisitedNodes = getAllNodes(grid);
 
+   while(!!unvisitedNodes.length) {
+       const sortedUnvisitedNodesByFCost = sortByFCost(unvisitedNodes)
+       const closestNode = sortedUnvisitedNodesByFCost.shift();
 
-    while (!!openList.length) {
-       const  sortedOpenListByFCost = sortOpenListByFCost(openList)        
-        // get minimum f-cost node
-       const closestNode = sortedOpenListByFCost.shift();
+       if (closestNode.isWall) contine;
 
-        //end case
-       if (closestNode === endNode) return closedList
+       if (closestNode.distance = Infinity) return visitedNodes;
 
-        // push closestNode onto closedList and remove from openList
-        closedList.push(closestNode)
-        closestNode.isVisted = true
+       closestNode.isVisted = true;
+       visitedNodes.push(closestNode)
 
-        const unvistedNeighbors = getUnvisitedNeighbors(closestNode, grid)
+       if (closestNode === endNode) return visitedNodes;
 
-        for (const neighbor of unvistedNeighbors) {
-            if (openList.includes(neighbor) || neighbor.isWall) continue;
-            // gCost is the shortest distance from start and closestNode
-            let gCost = closestNode.gCost + 1;
-            let isGCostBest = false;
-            
-            if (!openList.includes(neighbor)) {
-                //save g, h, f then save the currentParent
-                isGCostBest = true;
-                neighbor.hCost = manhattanHeuristic(startNode.x, startNode.y, endNode.x, endNode.y)
-                openList.push(neighbor)
-            } else if (gCost < neighbor.gCost) {
-                // We have already seen the node, but last time it had a worse g (distance from start)
-                isGCostBest = true; 
-            }
+       updateUnvisitedNeighbors(closestNode, grid)
+   }
+}
 
-            if (isGCostBest) {
-                neighbor.gCost = gCost
-                neighbor.fCost = neighbor.gCost + neighbor.hCost
-                neighbor.previousNode = closestNode;
-            }
-        }
+function updateUnvisitedNeighbors(node, grid) {
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid)
+    for (const neighbor of unvisitedNeighbors) {
+        neighbor.distance = node.distance + 1 //central node + 1 for surronding nodes
+        neighbor.previousNode = node //set point to backtrack
     }
 }
 
@@ -51,6 +35,6 @@ function manhattanHeuristic(startX, startY, endX, endY) {
     return Math.abs(startX - endX) + Math.abs(startY - endY)
 }
 
-function sortOpenListByFCost(openListArray) {
-    return openListArray.sort((nodeA,nodeB) => nodeA.fCost - nodeB.fCost)
+function sortByFCost(unvisitedNodes) {
+    return unvisitedNodes.sort((nodeA,nodeB) => nodeA.fCost - nodeB.fCost)
 }
