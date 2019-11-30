99862f7dad86a2df6237eb0c10802c83f292b9a1
jiminey <jtwnguyen@ucdavis.edu>
2019-11-30 04:39:39 -0800

    Comment on astar

--- src/Algorithms/astar.js
+++ src/Algorithms/astar.js
@@ -1,52 +1,49 @@
-import { getUnvisitedNeighbors } from "../Node/Node"
+import { getUnvisitedNeighbors } from "../Node/Node";
 
 export function astar(grid, startNode, endNode) {
-    // Initialize
-    const closedList = [];
-    startNode.gCost = 0;
-    startNode.fCost = 0;
-    startNode.hCost = 0;
-    endNode.hCost = 0
-    const openList = [startNode];
+  // Initialize
+  const closedList = [];
+  startNode.gCost = 0;
+  const openList = [startNode];
 
-    while(!!openList.length) {
-        sortNodesByFCost(openList)
-        const closestNode = openList.shift();
-        closedList.push(closestNode)
-        closestNode.isVisited = true;
+  while (!!openList.length) {
+    //sort nodes by F cost and grab min
+    sortNodesByFCost(openList);
+    const closestNode = openList.shift();
+    
+    closedList.push(closestNode);
+    closestNode.isVisited = true;
 
-
-        if (closestNode === endNode) return closedList; 
-        updateUnvisitedNeighbors(closestNode, grid, endNode, closedList, openList)
-    }
-    closedList.push({})
-    return closedList;
+    if (closestNode === endNode) return closedList;
+    updateUnvisitedNeighbors(closestNode, grid, endNode, closedList, openList);
+  }
+  closedList.push({});
+  return closedList;
 }
 
 function updateUnvisitedNeighbors(node, grid, endNode, closedList, openList) {
-    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid)
-    for (const neighbor of unvisitedNeighbors) {
-        // neighbor is in closedList
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    // neighbor is in closedList
+    if (closedList.includes(neighbor) || neighbor.isWall) continue;
 
-        if (closedList.includes(neighbor) || neighbor.isWall) continue; 
-    
-        neighbor.gCost = node.gCost + 1
-        neighbor.hCost = manhattanHeuristic(neighbor, endNode)
-        neighbor.fCost = neighbor.gCost + neighbor.hCost
-        neighbor.previousNode = node
+    neighbor.gCost = node.gCost + 1;
+    neighbor.hCost = manhattanHeuristic(neighbor, endNode);
+    neighbor.fCost = neighbor.gCost + neighbor.hCost;
+    neighbor.previousNode = node;
 
-        if (openList.includes(neighbor)) continue;
-        openList.push(neighbor);
-    }
+    // neighbor is in openList
+    if (openList.includes(neighbor)) continue;
+    openList.push(neighbor);
+  }
 }
 
 function manhattanHeuristic(node, endNode) {
-    // h-cost = distance from node to endNode
-    // Utilize the Manhattan Distance -- allows for 4 directional movement (up, down, left, right)
-    return Math.abs(node.row - endNode.row) + Math.abs(node.col - endNode.col)
-    
+  // h-cost = distance from node to endNode
+  // Utilize the Manhattan Distance -- allows for 4 directional movement (up, down, left, right)
+  return Math.abs(node.row - endNode.row) + Math.abs(node.col - endNode.col);
 }
 
 function sortNodesByFCost(openList) {
-    openList.sort((nodeA,nodeB) => nodeA.fCost - nodeB.fCost)
+  openList.sort((nodeA, nodeB) => nodeA.fCost - nodeB.fCost);
 }
--- src/Algorithms/dijkstras.js
+++ src/Algorithms/dijkstras.js
@@ -1,4 +1,4 @@
-import { getUnvisitedNeighbors } from "../Node/Node"
+import { getUnvisitedNeighbors } from "../Node/Node";
 
 //set distance to every node to infinity -- too far away
 //start node is distance of zero
@@ -7,52 +7,51 @@ import { getUnvisitedNeighbors } from "../Node/Node"
 //grab closest node of all remaing node, repeat
 
 export function dijkstra(grid, startNode, endNode) {
-    const visitedNodes = []
-    startNode.distance = 0
-    const unvistedNodes = getAllNodes(grid)
-    
-    //grab closest unvisited node and set it to visited and push to visitedNode until closest node 
-    while (!!unvistedNodes.length){
-        sortNodesByDistance(unvistedNodes)
-        const closestNode = unvistedNodes.shift();
+  const visitedNodes = [];
+  startNode.distance = 0;
+  const unvistedNodes = getAllNodes(grid);
 
-        //if encounter a wall we skip it aka move around
-        if (closestNode.isWall) continue;
+  //grab closest unvisited node and set it to visited and push to visitedNode until closest node
+  while (!!unvistedNodes.length) {
+    sortNodesByDistance(unvistedNodes);
+    const closestNode = unvistedNodes.shift();
 
-        //case of being trapped
-        if (closestNode.distance === Infinity) {
-            //edge case: add extra item to animate last obj when trapped
-            visitedNodes.push({})
-            return visitedNodes;
-        }
+    //if encounter a wall we skip it aka move around
+    if (closestNode.isWall) continue;
 
-        closestNode.isVisited = true;
-        visitedNodes.push(closestNode)
-
-        if (closestNode === endNode) return visitedNodes;
-        updateUnvisitedNeighbors(closestNode, grid);
+    //case of being trapped
+    if (closestNode.distance === Infinity) {
+      //edge case: add extra item to animate last obj when trapped
+      visitedNodes.push({});
+      return visitedNodes;
     }
+
+    closestNode.isVisited = true;
+    visitedNodes.push(closestNode);
+
+    if (closestNode === endNode) return visitedNodes;
+    updateUnvisitedNeighbors(closestNode, grid);
+  }
 }
 
 function updateUnvisitedNeighbors(node, grid) {
-  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid)
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
   for (const neighbor of unvisitedNeighbors) {
-    neighbor.distance = node.distance + 1 //central node + 1 for surronding nodes
-    neighbor.previousNode = node //set point to backtrack
+    neighbor.distance = node.distance + 1; //central node + 1 for surronding nodes
+    neighbor.previousNode = node; //set point to backtrack
   }
 }
 
 function sortNodesByDistance(unvistedNodes) {
-    unvistedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance)
+  unvistedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
 
 function getAllNodes(grid) {
-    const nodes = []; 
-    for (const row of grid) {
-        for (const node of row) {
-            nodes.push(node)
-        }
+  const nodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      nodes.push(node);
     }
-    return nodes
+  }
+  return nodes;
 }
-
