acf29370db334af8c71220edb67ece2424643265
jiminey <jtwnguyen@ucdavis.edu>
2019-11-30 04:53:37 -0800

    Implement remove and insert

--- src/DataStructures/heap.js
+++ src/DataStructures/heap.js
@@ -12,15 +12,21 @@ export default class MinHeap {
     siftUp(currentIdx, heap){
 
     }
+
     peek() {
         return this.heap[0]
     }
 
     remove() {
-
+        this.swap(0, this.heap.length - 1, this.heap)
+        let removedNode = this.heap.pop()
+        this.siftDown(0, this.heap.length - 1, this.heap)
+        return removedNode
     }
-    insert() { 
 
+    insert(value) { 
+        this.heap.push(value)
+        this.siftUp(this.heap.length - 1, this.heap)
     }
 
     swap(i ,j, heap) {
